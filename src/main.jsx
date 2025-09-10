90e11e0e1e406213ed59323524d4311f845bc0ad
Abdul Rehman <137405437+AbdulRehman938@users.noreply.github.com>
2025-09-10 20:29:40 +0500

    vercel page view

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes';
+import { inject } from '@vercel/analytics';
 import './index.css';
 import './App.css';
+
+// Initialize Vercel Analytics
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
